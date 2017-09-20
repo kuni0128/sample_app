f1e68be91d99694b3f73e27fd9c8f26c81428da4
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2017-09-20 17:02:21 +0900

    Change signup_path to use valid signup url

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,9 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' } }
+      post signup_path, params: { user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
+    assert_select 'form[action=?]', signup_path
     assert_select 'li', "Name can't be blank"
   end
 end
