30b07a9e65472d0eb196bc5601b6e8811563b25c
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2017-10-18 20:56:26 +0900

    Test micropost wrong user

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -12,10 +12,19 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test 'should redirect delete when not logged in' do
+  test 'should redirect destroy when not logged in' do
     assert_no_difference 'Micropost.count' do
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy for wrong micropost' do
+    log_in_as(users(:wettinton))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
